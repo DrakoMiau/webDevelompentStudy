bc5e69fbdf50c9edf2de340e47810fc26ad9e937
DrakoMiau <debernalg@unal.edu.co>
2025-03-29 23:19:41 -0500

    linked list working without addBefore and addAfter

--- back/dataStructures/cppProjects/linkedList/linkedList.cpp
+++ back/dataStructures/cppProjects/linkedList/linkedList.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <iostream>
+#include <ostream>
 
 // Clase Nodo
 
@@ -13,9 +14,13 @@ public:
 
   Node() {
     this->next = nullptr;
+    this->data = 0;
   }
 
-  Node(int data) { this->data = data; }
+  Node(int data) {
+    this->next = nullptr;
+    this->data = data;
+  }
 
   // getters y setters (encapsulamiento)
   int getData() { return this->data; }
@@ -28,33 +33,36 @@ public:
 };
 
 class LinkedList {
-
+private:
   // Atributos
-  Node *head;
+  Node *head = nullptr;
 
 public:
   // Constructores
-  LinkedList() {
-    this->head = nullptr;
-  }
+  LinkedList() { this->head = nullptr; }
 
   // Metodos
+
   void printList() {
     Node *ptr = this->head;
     if (ptr == nullptr) {
       std::cout << "La lista esta vacia" << std::endl;
     } else {
-      // el else es porque si ptr es nulo, no 
-      while (ptr->getNext() != nullptr) {
+      // el else es porque si ptr es nulo, no
+      while (ptr != nullptr) {
         std::cout << ptr->getData() << std::endl;
+        ptr = ptr->getNext();
       }
     }
   }
+
   void pushBack(int n) {
     Node *ptr = this->head;
-    Node *newNode = new Node(n);
+    Node *newNode = new Node(
+        n); // si solo declaro Node newnode, tengo el problema de que es una
+            // variable local y muere cuando se termine la funcion
     if (this->head == nullptr) {
-      this->head->setData(n);
+      this->head = newNode;
     } else {
       while (ptr->getNext() != nullptr) {
         ptr = ptr->getNext();
@@ -62,16 +70,106 @@ public:
       ptr->setNext(newNode);
     }
   }
-  void pushFront(int n) {}
-  void pop(int n) {}
+
+  void pushFront(int n) {
+    Node *newNode = new Node(n);
+    if (this->head == nullptr) {
+      this->head = newNode;
+    } else {
+      newNode->setNext(this->head);
+      this->head = newNode;
+    }
+  }
+  Node *popFront() {
+    if (this->head == nullptr) {
+      std::cout << "La lista esta vacia" << std::endl;
+      return nullptr;
+    } else {
+      this->head = this->head->getNext();
+      return head;
+    }
+  }
+  Node *popBack() {
+    Node *ptr = this->head;
+    Node *prev = ptr;
+    if (this->head == nullptr) {
+      std::cout << "La lista esta vacia" << std::endl;
+      return nullptr;
+    } else {
+      while (ptr->getNext() != nullptr) {
+        prev = ptr;
+        ptr = ptr->getNext();
+      }
+      prev->setNext(nullptr);
+      return ptr;
+    }
+  }
+  Node *topFront() {
+    if (this->head == nullptr) {
+      std::cout << "La lista esta vacia" << std::endl;
+    }
+    return head;
+  }
+  Node *topBack() {
+    Node *ptr;
+    if (this->head == nullptr) {
+      std::cout << "La lista esta vacia" << std::endl;
+      return head;
+    } else {
+      while (ptr->getNext() != nullptr) {
+        ptr = ptr->getNext();
+      }
+      return ptr;
+    }
+  }
+  bool Find(int key) {
+    Node *ptr = this->head;
+    bool isIn = false;
+    while (ptr != nullptr) {
+      ptr = ptr->getNext();
+      if (ptr->getData() == key) {
+        isIn = true;
+      }
+    }
+    return isIn;
+  }
+  bool isEmpty() {
+    if (this->head != nullptr) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // Los siguientes metodos son para el primer dato que coincida
+
+  void addBefore(Node, int key) {
+
+  }
+  void addAfter(int key) {}
+  void erase(int key) {}
 };
 
 int main() {
+  LinkedList myList;
+  std::cout << myList.isEmpty() << std::endl;
   int n;
   std::cout << "Ingrese un numero entero: ";
   std::cin >> n;
-  LinkedList myList;
-  myList.pushBack(n);
+  std::cout << std::endl;
+  myList.pushFront(n);
+  std::cout << "Ingrese un numero entero: ";
+  std::cin >> n;
+  std::cout << std::endl;
+  myList.pushFront(n);
+  std::cout << "Ingrese un numero entero: ";
+  std::cin >> n;
+  std::cout << std::endl;
+  myList.pushFront(n);
   myList.printList();
+  myList.popBack();
+  myList.printList();
+  std::cout << "Veamos si la lista esta vacia: " << myList.isEmpty()
+            << std::endl;
   return 0;
 }
