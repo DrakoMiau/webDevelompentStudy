68f6c7908c1957953d0eb1ef5dece62475845759
DrakoMiau <debernalg@unal.edu.co>
2025-03-31 13:18:08 -0500

    ordered linked list with a little bug

--- back/dataStructures/javaProjects/OrderedLinkedList/src/Main.java
+++ back/dataStructures/javaProjects/OrderedLinkedList/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        OrderedLinkedList myList = new OrderedLinkedList();
+        // para 2 funciona bien
+        myList.insert(2);
+        myList.insert(1);
+        myList.insert(4);
+        myList.insert(3);
+        myList.printList();
     }
 }
--- back/dataStructures/javaProjects/OrderedLinkedList/src/OrderedLinkedList.java
+++ back/dataStructures/javaProjects/OrderedLinkedList/src/OrderedLinkedList.java
@@ -27,7 +27,49 @@ public class OrderedLinkedList {
 
     // Metodos
 
-    public void insert(int n){
+    public void printList(){
+        Node ptr = this.head;
+        if (ptr == null) {
+            System.out.println("La lista esta vacia");
+        } else {
+            while(ptr != null){
+                System.out.println(ptr.getData());
+                ptr = ptr.getNext();
+            }
+        }
 
+    }
+
+    public void insert(int n){
+        Node ptr = head;
+        Node newNode = new Node(n);
+        if (ptr == null){
+            head = newNode;
+            return;
+        }
+
+        if (ptr.getNext() == null) {
+            if (n <= ptr.getData()){
+                head = newNode;
+                head.setNext(ptr);
+                return;
+            } else {
+                head.setNext(newNode);
+            }
+        }else {
+            while (ptr != null) {
+                if (ptr.getData() > n && n < ptr.getNext().getData()) {
+                    // Ubico a newNode entre ptr y ptr.getNext()
+                    Node temp = ptr.getNext();
+                    ptr.setNext(newNode);
+                    newNode.setNext(temp);
+                    return;
+                } else if (ptr.getNext() == null) {
+                    ptr.setNext(newNode);
+                    return;
+                }
+                ptr = ptr.getNext();
+            }
+        }
     }
 }
