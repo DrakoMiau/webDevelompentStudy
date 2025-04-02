3e27507ce959c7ecf9bfdb0c490dc1de8afc5727
DrakoMiau <debernalg@unal.edu.co>
2025-04-01 22:26:41 -0500

    ordered linked list finished (is literally the insertion sort)

--- back/dataStructures/javaProjects/OrderedLinkedList/src/Main.java
+++ back/dataStructures/javaProjects/OrderedLinkedList/src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         OrderedLinkedList myList = new OrderedLinkedList();
-        // para 2 funciona bien
         myList.insert(2);
         myList.insert(1);
         myList.insert(4);
-        myList.insert(3);
+        myList.insert(2);
+        myList.insert(10);
+        myList.insert(6);
+        myList.insert(6);
+        myList.insert(0);
         myList.printList();
     }
 }
--- back/dataStructures/javaProjects/OrderedLinkedList/src/OrderedLinkedList.java
+++ back/dataStructures/javaProjects/OrderedLinkedList/src/OrderedLinkedList.java
@@ -41,31 +41,36 @@ public class OrderedLinkedList {
     }
 
     public void insert(int n){
-        Node ptr = head;
+        Node ptr = this.head;
         Node newNode = new Node(n);
-        if (ptr == null){
-            head = newNode;
+        if (ptr == null){ // si no tiene elementos
+            this.head = newNode;
             return;
         }
 
-        if (ptr.getNext() == null) {
+        if (ptr.getNext() == null) { // si tiene solo un elemento
             if (n <= ptr.getData()){
-                head = newNode;
-                head.setNext(ptr);
+                this.head = newNode;
+                this.head.setNext(ptr);
                 return;
             } else {
-                head.setNext(newNode);
+                this.head.setNext(newNode);
             }
-        }else {
+        }else { // si tiene dos o mas elementos
             while (ptr != null) {
-                if (ptr.getData() > n && n < ptr.getNext().getData()) {
+                if (ptr.getNext() != null && ptr.getData() <= n && n <= ptr.getNext().getData()) { // caso en el que existe un elemento mayor y otro menor que newNode y que no sea el ultimo
                     // Ubico a newNode entre ptr y ptr.getNext()
                     Node temp = ptr.getNext();
                     ptr.setNext(newNode);
                     newNode.setNext(temp);
                     return;
-                } else if (ptr.getNext() == null) {
+                } else if (ptr.getNext() == null && ptr.getData() <= n) { // caso en el que newNode es mayor que todos
                     ptr.setNext(newNode);
+                    return;
+                } else if(ptr.getNext() == null && ptr.getData() >= n){ // caso en el que newNode es menor que todos
+                    ptr = this.head;
+                    this.head = newNode;
+                    this.head.setNext(ptr);
                     return;
                 }
                 ptr = ptr.getNext();
